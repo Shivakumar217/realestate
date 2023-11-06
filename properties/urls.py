aaef6633d738954b2747043d6fa46e580ed6ab11
shiva kumar <bubby@shivas-MacBook-Pro.local>
2023-11-06 15:29:44 -0500

    Add Homepage app

--- properties/urls.py
+++ properties/urls.py
@@ -1,23 +1,2 @@
 # properties/urls.py
 from django.urls import path
-from .views import (
-<<<<<<< HEAD
-    
-    PropertyCreateView,
-    rate_property,
-    
-)
-
-urlpatterns = [
-    
-=======
-    PropertyCreateView,
-    rate_property,
-   )
-
-urlpatterns = [
->>>>>>> fe8bbefd688c366333bc171f991201c5c97085b3
-    path('property/add/', PropertyCreateView.as_view(), name='property-create'),
-    path('property/rate/<int:property_id>/', rate_property, name='rate-property'),
-   
-]
--- properties/views.py
+++ properties/views.py
@@ -1,19 +1,3 @@
 import json
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Property
-from django.views.generic import CreateView
-    
-class PropertyCreateView(CreateView):
-    model = Property
-    template_name = 'properties/property_form.html'
-    fields = ['title', 'description', 'price', 'is_sold', 'rating']
-    success_url = '/properties/'
-
-
-def rate_property(request, property_id):
-    property = get_object_or_404(Property, pk=property_id)
-    if request.method == 'POST':
-        rating = request.POST.get('rating')
-        property.rating = rating
-        property.save()
-    return redirect('property-list')
