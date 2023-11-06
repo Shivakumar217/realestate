c6cf6ce87d735fa8d55eae4708b9e6d6f58090c8
shiva kumar <bubby@shivas-MacBook-Pro.local>
2023-11-06 14:44:34 -0500

    Add homepage app

--- properties/urls.py
+++ properties/urls.py
@@ -1,15 +1,3 @@
 # properties/urls.py
 from django.urls import path
-from .views import (
-    
-    PropertyCreateView,
-    rate_property,
-    
-)
 
-urlpatterns = [
-    
-    path('property/add/', PropertyCreateView.as_view(), name='property-create'),
-    path('property/rate/<int:property_id>/', rate_property, name='rate-property'),
-   
-]
